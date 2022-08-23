1b666691f931ae99dcc7595775c88325e112effe
Priyamvada <50589695+Priyamvada03@users.noreply.github.com>
2022-08-23 15:36:23 +0530

    Time: 1 ms (89.64%), Space: 43.3 MB (64.04%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -20,23 +20,3 @@ class Solution {
 
 
 
-
-
-
-// class Solution {
-//     public List<List<Integer>> subsets(int[] nums) {
-//         List<List<Integer>> ll=new ArrayList<>();
-//         isSets(0,nums,ll,new ArrayList<>());
-//         return ll;
-//     }
-//     public void isSets(int index,int[] nums,List<List<Integer>> ll, ArrayList<Integer> l){
-//         if(index==nums.length){
-//             ll.add(new ArrayList<>(l));
-//             return;
-//         }
-//         l.add(nums[index]);
-//         isSets(index+1,nums,ll,l);
-//         l.remove(l.size()-1);
-//         isSets(index+1,nums,ll,l);
-//     }
-// }
