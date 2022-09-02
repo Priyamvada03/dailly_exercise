c193406c04bbad92ec6a183d8ea21eeceb240f58
Priyamvada <50589695+Priyamvada03@users.noreply.github.com>
2022-09-02 20:27:36 +0530

    Time: 4 ms (86.77%), Space: 44.9 MB (80.93%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] a, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ls = new ArrayList<>();  
-        combSum(0,ans,a,target,ls); 
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        trace(ans,temp,target,0,a);
         return ans;
     }
-    public static void combSum(int idx,List<List<Integer>> ans,int[]a,int tar,List<Integer> ls){
-      
-        if(idx==a.length){
-            if(tar==0){
-            ans.add(new ArrayList<>(ls));
-            
-        }
+    
+    public static void trace(List<List<Integer>> ans,List<Integer> temp,int tar,int i,int[] a){
+        if(tar==0){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        if(tar-a[idx]>=0){
-            ls.add(a[idx]);
-            combSum(idx,ans,a,tar-a[idx],ls);
-            ls.remove(ls.size()-1);
+        if(i==a.length){
+            return;
         }
-        
-        combSum(idx+1,ans,a,tar,ls);
+        if(tar-a[i]>=0){
+            temp.add(a[i]);
+            trace(ans,temp,tar-a[i],i,a);
+            temp.remove(temp.size()-1);
+        }
+         trace(ans,temp,tar,i+1,a);
     }
 }
