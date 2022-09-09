9b7d8ca3171d6e66acb8934e8a083f001925fbb6
Priyamvada <50589695+Priyamvada03@users.noreply.github.com>
2022-09-09 18:18:33 +0530

    Time: 44 ms (7.03%), Space: 72.4 MB (5.11%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
@@ -1,26 +1,22 @@
-
-
 class Solution {
-    public int maxSizeSlices(int[] nums) {
-        int k=nums.length/3;
-        int[] a1=Arrays.copyOfRange(nums,1,nums.length);
-        int[] a2=Arrays.copyOfRange(nums,0,nums.length-1);
+    public int maxSizeSlices(int[] slices) {
+        int k=slices.length/3;
+        int[] a1=Arrays.copyOfRange(slices,1,slices.length);
+        int[] a2=Arrays.copyOfRange(slices,0,slices.length-1);
         int[][] dp1=new int[501][170];
         int[][] dp2=new int[501][170];
         for(int i=0;i<501;i++){
             for(int j=0;j<170;j++){
-                       dp1[i][j]=-1;
+            dp1[i][j]=-1;
             dp2[i][j]=-1;
             }
         }
       return Math.max(robbery(a1,dp1,0,k),robbery(a2,dp2,0,k));
-        
     }
     public static int robbery(int[] a,int[][] dp,int i,int k){
         if(i>=a.length || k==0){
             return 0;
         }
-      
         if(dp[i][k]!=-1){
             return dp[i][k];
         }
