f72bb05fb7a8718ebcf7663c0c48e0e41d6f2ad9
Priyamvada <50589695+Priyamvada03@users.noreply.github.com>
2022-09-09 18:16:33 +0530

    Time: 19 ms (26.20%), Space: 43.3 MB (84.34%) - LeetHub

---
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
@@ -0,0 +1,29 @@
+
+
+class Solution {
+    public int maxSizeSlices(int[] nums) {
+        int k=nums.length/3;
+        int[] a1=Arrays.copyOfRange(nums,1,nums.length);
+        int[] a2=Arrays.copyOfRange(nums,0,nums.length-1);
+        int[][] dp1=new int[501][170];
+        int[][] dp2=new int[501][170];
+        for(int i=0;i<501;i++){
+            for(int j=0;j<170;j++){
+                       dp1[i][j]=-1;
+            dp2[i][j]=-1;
+            }
+        }
+      return Math.max(robbery(a1,dp1,0,k),robbery(a2,dp2,0,k));
+        
+    }
+    public static int robbery(int[] a,int[][] dp,int i,int k){
+        if(i>=a.length || k==0){
+            return 0;
+        }
+      
+        if(dp[i][k]!=-1){
+            return dp[i][k];
+        }
+        return dp[i][k]= Math.max(robbery(a,dp,i+2,k-1)+a[i],robbery(a,dp,i+1,k));
+    }
+}
